ea565b298b5be0e60c204edfbab192ce54d4abfa
rjiang@caltech.edu <rjiang@caltech.edu>
2022-05-19 19:14:21 +0000

    used time.h...

--- include/powerups.h
+++ include/powerups.h
@@ -19,20 +19,38 @@ const char FREEZE_ENEMY = 'f';
 const char DOUBLE_GOAL = 'g';
 
 void double_velocity(body_t *puck) {
-  vector_t curr_velocity = body_get_velocity(puck); 
-  body_set_velocity(puck, vec_multiply(2, curr_velocity));
+  time_t start = time(NULL);
+  time_t now = time(NULL); 
+  while((now - start) < 5) {
+    vector_t curr_velocity = body_get_velocity(puck); 
+    body_set_velocity(puck, vec_multiply(2, curr_velocity));
+  }
+  
 }
 
 void double_accel(body_t *last_touch) {
-  body_set_velocity(last_touch, vec_multiply(2.0, body_get_velocity(last_touch))); 
+  time_t start = time(NULL);
+  time_t now = time(NULL); 
+  while((now - start) < 5) {
+    body_set_velocity(last_touch, vec_multiply(2.0, body_get_velocity(last_touch))); 
+  }
+  
 }
 
 void half_accel(body_t *other_player) {
-  body_set_velocity(other_player, vec_multiply(0.5, body_get_velocity(other_player))); 
+  time_t start = time(NULL);
+  time_t now = time(NULL); 
+  while((now - start) < 5) {
+    body_set_velocity(other_player, vec_multiply(0.5, body_get_velocity(other_player))); 
+  }
 }
 
 void freeze_enemy(body_t *other_player) {
-  body_set_velocity(other_player, (vector_t) {0, 0});
+  time_t start = time(NULL);
+  time_t now = time(NULL); 
+  while((now - start) < 5) {
+    body_set_velocity(other_player, (vector_t) {0, 0});
+  }
 }
 
 void double_goal(state_t *state) {
@@ -50,6 +68,7 @@ void powerup_collision(state_t *state, body_t *puck, body_t *powerup) {
     case FREEZE_ENEMY:
       freeze_enemy(state->other_player);
     case DOUBLE_GOAL:
+      double_goal(state); 
     default:
       return; 
   }
